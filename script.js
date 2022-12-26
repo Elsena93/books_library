5dce3747e11fbf90cfc43cd35c474d02ae0cdf65
Elsena93 <elsena.kurniadi@gmail.com>
2022-12-26 15:02:13 +0700

    fix: change all function based to class based

--- script.js
+++ script.js
@@ -1,153 +1,187 @@
-let myLibrary = [];
-const rack = document.querySelector("main");
-
-// Book constructor
-function Book (title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
-
-// Prototype function
-Book.prototype.showInfo = function () {
-    let info = this.title + ' by ' + this.author + ', ' + this.pages + ' pages, me ' + this.read + ' read';
-    return info;
-}
-
-// Function to add new book to library
-function addBook (title, author, pages, read) {
-    let newBook = new Book (title, author, pages, read);
-    myLibrary.push(newBook);
-}
-    
-// Function to show one book to display
-function displayBook (book) {
-    const protoBook = document.createElement("div");
-    const bookTitle = document.createElement("span");
-    const bookAuthor = document.createElement("span");
-    let bookIndex = myLibrary.indexOf(book);
-
-    protoBook.classList = "book";
-    protoBook.setAttribute('data-key', bookIndex);
-    bookAuthor.classList = "author";
-    bookTitle.classList = "title";
-
-    bookTitle.innerText = book.title;
-    bookAuthor.innerText = book.author;
-    protoBook.appendChild(bookTitle);
-    protoBook.appendChild(bookAuthor);
-
-    // Add read button
-    const readButton = document.createElement("button");
-    readButton.classList = "read";
-    if (book.read == 'yes') {
-        readButton.innerText = "READ";
-    } else {
-        readButton.innerText = "NOT READ";
+// Library class
+class Library {
+    constructor() {
     }
-    // Event Listener for read button
-    readButton.addEventListener("click", function () {
-        changeRead(book);
-        readDisplay(bookIndex);
-    });
 
-    // Add remove button
-    const removeButton = document.createElement("button");
-    removeButton.classList = "remove";
-    removeButton.innerText = "REMOVE";
-    // Event Listener for remove button
-    removeButton.addEventListener("click", function () {
-        hideBook(bookIndex);
-        removeBook(bookIndex);
-    });
+    bookStack = [];
 
-    protoBook.appendChild(readButton);
-    protoBook.appendChild(removeButton);
-    rack.appendChild(protoBook);
-}
+    get bookList() {
+        console.log(this.bookStack);
+    }
 
-// Function to change read status in library, it comes first before changing diplay
-function changeRead (book) {
-    if (book.read == 'yes') {
-        book.read = 'no';
-    } else {
-        book.read = 'yes';
+    set addBook(book) {
+        this.bookStack.push(book);
+    }
+
+    set removeBook(book) {
+        this.bookStack.splice(this.bookStack.indexOf(book), 1);
     }
 }
 
-// Function to change read status in diplay
-function readDisplay (bookIndex) {
-    const displayBook = document.querySelector(`div[data-key="${bookIndex}"] .read`);
-    if (myLibrary[bookIndex].read == 'yes') {
-        displayBook.innerText = "READ";
-    } else {
-        displayBook.innerText = "NOT READ";
+// Book class, object to pass around
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+
+    get readStatus() {
+        return this.read;
+    }
+
+    set readStatus(input) {
+        this.read = input;
+        return this.read;
+    }
+
+    get getElement() {
+        const protoBook = document.createElement("div");
+        const bookTitle = document.createElement("span");
+        const bookAuthor = document.createElement("span");
+
+        protoBook.classList = "book";
+        bookAuthor.classList = "author";
+        bookTitle.classList = "title";
+        
+        bookTitle.innerText = this.title;
+        bookAuthor.innerText = this.author;
+        protoBook.appendChild(bookTitle);
+        protoBook.appendChild(bookAuthor);
+
+        // Add read button
+        const readButton = document.createElement("button");
+        readButton.classList = "read";
+        if (this.read == 'yes') {
+            readButton.innerText = "READ";
+        } else {
+            readButton.innerText = "NOT READ";
+        }
+        protoBook.appendChild(readButton);
+
+        // Add remove button
+        const removeButton = document.createElement("button");
+        removeButton.classList = "remove";
+        removeButton.innerText = "REMOVE";
+        protoBook.appendChild(removeButton);
+
+        return protoBook;
     }
 }
 
-// Function to remove one book from display, it comes first before changing diplay
-function hideBook (bookIndex) { 
-    const hidBook = document.querySelector(`div[data-key="${bookIndex}"]`);
-    rack.removeChild(hidBook);
-}
-
-// Function to remove book from library
-function removeBook (bookIndex) {
-    myLibrary.splice(bookIndex, 1);
-}
-
-// Function to show form with button
-const formContainer = document.querySelector(".form");
-const formButton = document.querySelector(".addNew");
-
-formButton.addEventListener("click", function() {
-    formContainer.style.display = "block";
-})
-
-// Function of submit on form
-const submitButton = document.querySelector("form button");
-const inputTitle = document.querySelector("#form-title");
-const inputAuthor = document.querySelector("#form-author");
-const inputPages = document.querySelector("#form-pages");
-const inputRead = document.querySelector("#form-read");
-
-submitButton.addEventListener("click", function (e) {
-    //Check first no input is empty
-    if (inputTitle.value == '' || 
-        inputAuthor.value == ''||
-        inputPages.value == ''||
-        inputRead.value == '') {
-    alert("please fill all field");
-    } else {
-        addBook(inputTitle.value, inputAuthor.value, inputPages.value, inputRead.value);
-        displayBook(myLibrary[myLibrary.length-1]);
-        //Reset all input value to empty
-        inputTitle.value = '';
-        inputAuthor.value = '';
-        inputPages.value= '';
-        inputRead.value = '';
-        // Close the form
-        formContainer.style.display = "none";
+//Interacting with library from display
+class LibaryDisplay extends Library {
+    constructor() {
+    super()
     }
 
-    e.preventDefault();
-})
+    container = document.querySelector('main');
 
-// Adding sample books
+    changeReadStatus = (e) => {
+        let bookDisplay = e.target.parentNode;
+        let book = this.bookStack[bookDisplay.dataset.index];
+        if (book.readStatus === 'yes') {
+            book.readStatus = 'no';
+            e.target.innerText = 'NOT READ';
+        } else {
+            book.readStatus = 'yes';
+            e.target.innerText = 'READ';
+        }
+    }
 
+    removeButton = (e) => {
+        let bookDisplay = e.target.parentNode;
+        let book = this.bookStack[bookDisplay.dataset.index];
+        this.removeBookDisplay = book;
+        this.removeBook = book;
+        this.updateElementIndex();  
+    }
+
+    //When a book removed from display, its data-index attribute need to be updated
+    updateElementIndex = () => {
+        let index = 0;
+        Array.from(this.container.children).forEach(element => {
+            element.dataset.index = index;
+            index += 1;
+        });
+    }
+
+    set addBookDisplay(book) {
+        let protoBook = book.getElement;
+        protoBook.setAttribute('data-index', this.bookStack.indexOf(book));
+        protoBook.querySelector('.read').addEventListener('click', this.changeReadStatus);
+        protoBook.querySelector('.remove').addEventListener('click', this.removeButton);
+        this.container.appendChild(protoBook);
+    }
+
+    set removeBookDisplay(book) {
+        let bookIndex = this.bookStack.indexOf(book);
+        let removeBook = document.querySelector(`[data-index="${bookIndex}`);
+        this.container.removeChild(removeBook);
+    }
+
+}
+
+//Add book via form
+class BookForm {
+    constructor(){
+    }
+
+    submitFunction = (e) => {
+        //Check first no input is empty
+        if (this.inputTitle.value == '' || 
+            this.inputAuthor.value == ''||
+            this.inputPages.value == ''||
+            this.inputRead.value == '') {
+            alert("please fill all field");
+        } else {
+            let newBook = new Book (this.inputTitle.value, this.inputAuthor.value, this.inputPages.value, this.inputRead.value);
+            console.log(newBook);   
+            myDisplay.addBook = newBook;
+            myDisplay.addBookDisplay = newBook;
+            //Reset all input value to empty
+            this.inputTitle.value = '';
+            this.inputAuthor.value = '';
+            this.inputPages.value= '';
+            this.inputRead.value = '';
+            // Close the form
+            this.formContainer.style.display = "none";
+        }
+
+        e.preventDefault();
+    };
+
+    formContainer = document.querySelector(".form");
+    formButton = document.querySelector(".addNew").addEventListener('click', () => {this.formContainer.style.display = "block";});
+
+    submitButton = document.querySelector('form button').addEventListener('click', this.submitFunction);
+    inputTitle = document.querySelector("#form-title");
+    inputAuthor = document.querySelector("#form-author");
+    inputPages = document.querySelector("#form-pages");
+    inputRead = document.querySelector("#form-read");
+
+}
+
+
+//Initiate Display
+let myDisplay = new LibaryDisplay();
+
+let myForm = new BookForm();
+
+// Adding sample books in class
 let sample1 = new Book ('The Hobbit #1', 'Tolkienish', 1001, 'no');
 let sample2 = new Book ('LOTR #1', 'Tookien', 1002, 'no');
 let sample3 = new Book ('LOTR #2', 'Tolkien', 1003, 'yes');
 let sample4 = new Book ('LOTR #3', 'Tolkien too', 1004, 'yes');
 let sample5 = new Book ('The Hobbit #2', 'Tolkien', 1005, 'no');
 
-myLibrary.push(sample1);
-myLibrary.push(sample2);
-myLibrary.push(sample3);
-myLibrary.push(sample4);
-myLibrary.push(sample5);
+myDisplay.addBook =  sample1;
+myDisplay.addBook =  sample2;
+myDisplay.addBook =  sample3;
+myDisplay.addBook =  sample4;
+myDisplay.addBook =  sample5;
 
-myLibrary.forEach(element => {
-    displayBook(element);
-});
+myDisplay.bookStack.forEach(book => {
+    myDisplay.addBookDisplay = book;
+}); 
